e484f99d16edb57b8959ed3f5a4907ec2c762b73
Akın Tanrıverdi <akntanrverdi2@gmail.com>
2025-05-12 01:46:54 +0300

    CV modeli oluşturuldu ve migrate edildi

--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class CV(models.Model):
+    first_name = models.CharField(max_length=100, default="Akın")
+    last_name = models.CharField(max_length=100, default="Tanrıverdi")
+    email = models.EmailField(default="akntanrverdi2@gmail.com")
+    phone_number = models.CharField(max_length=20, default="05380891401")
+    job_title = models.CharField(max_length=100, default="Information Systems Engineer")
+    bio = models.TextField(default="Passionate about technology and development, with a focus on IT systems. Enthusiastic learner and problem solver.")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"

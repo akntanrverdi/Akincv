591385dbcf3e28ed90d0243fc517e391cc4e0631
Akın Tanrıverdi <akntanrverdi2@gmail.com>
2025-05-12 01:49:57 +0300

    Ana sayfa ve CV gösterimi eklendi

--- cv/views.py
+++ cv/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import CV
 
-# Create your views here.
+def home(request):
+    cv_data = CV.objects.first()
+    return render(request, 'home.html', {'cv_data': cv_data})
